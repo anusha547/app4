e60006adcd6a53531890330f1622239fb96c15ed
anu <anusha547@gmail.com>
2016-12-15 07:44:28 +0530

    First commit

---
+++ src/main/java/com/example/App4Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App4Application.class, args);
+	}
+	
+	
+}
